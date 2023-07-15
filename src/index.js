31c850f8e705a2e0b44d362bd8373bd8e5c69a88
Ravindra Nath Jha <ravindranathjha76@gmail.com>
2023-07-15 19:53:49 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode basename={process.env.PUBLIC_URL}>
     <App />
   </React.StrictMode>
 );
